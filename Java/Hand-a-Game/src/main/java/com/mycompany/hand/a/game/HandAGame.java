01ee2d852ef45b394b9d2d8a0b098f8f1f11ba4e
Amaro <tpedro.amaro@gmail.com>
2023-02-24 08:59:09 -0300

    creates user class

---
+++ Java/Hand-a-Game/src/main/java/com/mycompany/hand/a/game/HandAGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hand.a.game;
+
+/**
+ *
+ * @author Amaro
+ */
+public class HandAGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
