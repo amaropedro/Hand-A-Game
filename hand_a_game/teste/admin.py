e4874475bf12909cc43fc2013e7b8c12f49fd0d1
AmaroLaptop <tpedro.amaro@gmail.com>
2023-11-07 15:34:27 -0300

    FEAT: adds platform, game and genres models

--- hand_a_game/teste/admin.py
+++ hand_a_game/teste/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import *
 
 admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Platform)
+admin.site.register(Genres)
--- hand_a_game/teste/models.py
+++ hand_a_game/teste/models.py
@@ -7,9 +7,36 @@ import datetime
 class User(models.Model):
   name = models.CharField(max_length=200)
   email = models.EmailField()
-  city = models.CharField()
-  contact = models.CharField()
-  password = models.CharField()
+  city = models.CharField(max_length=200)
+  contact = models.CharField(max_length=200)
+  password = models.CharField(max_length=200)
 
   def __str__(self):
     return self.name
+
+class Platform(models.Model):
+  platformName = models.CharField(max_length=200)
+  allowsSimultaneousRentals = models.BooleanField()
+  
+  def __str__(self):
+    return self.platformName
+
+class Game(models.Model):
+  title = models.CharField(max_length=200)
+  isRented = models.BooleanField()
+  isPhysical = models.BooleanField()
+  cover =  models.ImageField()
+  rentalDuration = models.IntegerField()
+  platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.title
+
+class Genres(models.Model):
+  game = models.ForeignKey(Game, on_delete=models.CASCADE)
+  genreName = models.CharField(max_length=200)
+  
+  def __str__(self):
+    return self.genreName
+  
