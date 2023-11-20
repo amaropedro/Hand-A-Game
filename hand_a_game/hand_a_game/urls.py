eb7c7f540abb3602508d06d778ca263532c97bfc
AmaroLaptop <tpedro.amaro@gmail.com>
2023-11-20 10:50:10 -0300

    FEAT: adds test view

--- hand_a_game/hand_a_game/urls.py
+++ hand_a_game/hand_a_game/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', main.views.listTest, name="list")
 ]
--- hand_a_game/main/views.py
+++ hand_a_game/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Game
+
 # Create your views here.
+def listTest(request):
+    allGames = Game.objects.order_by('user')
+    context = {'games_list': allGames}
+    return render(request, 'main/index.html', context)
