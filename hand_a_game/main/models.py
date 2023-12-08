6c40400de8426be1d344d0ae15938a08da6bf585
AmaroLaptop <tpedro.amaro@gmail.com>
2023-12-08 15:44:32 -0300

    FIX: rmv unused library

--- hand_a_game/main/models.py
+++ hand_a_game/main/models.py
@@ -5,7 +5,6 @@ import os
 
 from django.db import models
 from django.dispatch import receiver
-from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 import datetime
